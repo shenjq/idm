44c7929e06917fd6fe0534deb8bb8ad1c807b1f9
shenjq <rock_jq@163.com>
2020-03-05 14:46:25 +0800

    update read config file

--- conf/rdconf.go
+++ conf/rdconf.go
@@ -1,10 +1,16 @@
 package conf
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/golang/glog"
 	"gopkg.in/ini.v1"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 type DbInfo struct { //结构体名称与配置文件section名称必须一致
@@ -36,20 +42,39 @@ var gConfFile string
 func init() {
 	currpath, err := getCurrentPath()
 	if err != nil {
-		glog.V(0).Infof("GetCurrentPath err,%v", err)
+		fmt.Printf("GetCurrentPath err,%v", err)
 	}
 	defaultCfgFile := fmt.Sprintf("%sconf/conf.ini", currpath)
 	flag.StringVar(&gConfFile, "cfgfile", defaultCfgFile, "config file,default path:"+defaultCfgFile)
 }
 func getCurrentPath() (string, error) {
-	return "", nil
+	file, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		return "", err
+	}
+	path, err := filepath.Abs(file)
+	if err != nil {
+		return "", err
+	}
+
+	if runtime.GOOS == "windows" {
+		path = strings.Replace(path, "\\", "/", -1)
+	}
+
+	i := strings.LastIndex(path, "/")
+	if i < 0 {
+		return "", errors.New(`Can't find "/" or "\".`)
+	}
+
+	return string(path[0 : i+1]), nil
 }
 
 func InitConf() (err error) {
 	//cfg, _ := ini.Load("/Users/shenjq/go/src/demo/idm/conf/conf.ini")
 	//err = cfg.MapTo(gIni)
-	err = ini.MapTo(gIni, "/Users/shenjq/go/src/demo/idm/conf/conf.ini")
+	err = ini.MapTo(gIni, gConfFile)
 	if err != nil {
+		fmt.Printf("getConfigfile err,%v", err)
 		glog.V(0).Infof("initconf err,%v", err)
 		return
 	}

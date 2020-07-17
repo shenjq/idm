7099bf26290e38552cdbf2927c2ad3815a605bfc
shenjq <rock_jq@163.com>
2020-07-17 12:00:50 +0800

    add func

--- pub/pub.go
+++ pub/pub.go
@@ -1,6 +1,13 @@
 package pub
 
-import "time"
+import (
+	"github.com/golang/glog"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
 
 const (
 	timeTemplate1 = "2006-01-02 15:04:05" //常规类型
@@ -56,4 +63,43 @@ func IsYearEnd(s string) (bool, error) {
 
 func GetCurrentPath() (string, error) {
 	return "", nil
+}
+
+
+//dataformat id=1000001&value=20.4&host=ipaddress
+func PostForm(url string, data string) (string, error) {
+	contentType := "application/x-www-form-urlencoded"
+	return post(url, contentType, strings.NewReader(data))
+}
+
+//dataformat json.Unmarshal([]byte(jsonStr), &data) ##data struct
+func PostJson(url, data string) (string, error) {
+	contentType := "application/json"
+	//jsonStr, _ := json.Marshal(data)
+	//return post(url, contentType, bytes.NewReader(jsonStr))
+	return post(url, contentType, strings.NewReader(data))
+}
+
+// 发送POST请求
+// url：         请求地址
+// body：        POST请求提交的数据
+// contentType： 请求体格式，如：application/json,application/x-www-form-urlencoded
+// return：     请求放回的内容
+func post(url string, contentType string, body io.Reader) (string, error) {
+
+	//fmt.Printf("[%v]\n", body)
+	// 超时时间：5秒
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Post(url, contentType, body)
+	if err != nil {
+		glog.V(0).Infof("http post err,%v\n", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
 }
